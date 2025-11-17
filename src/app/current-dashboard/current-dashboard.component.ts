78150afc91d570f600a2b9f4ab933f6e36cdf3e9
Finagnon Romuald SONOU <frsonou@ic-hn-services.center>
2025-11-17 14:20:33 +0100

    commit on branch main

--- src/app/current-dashboard/current-dashboard.component.ts
+++ src/app/current-dashboard/current-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-current-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './current-dashboard.component.html',
   styleUrl: './current-dashboard.component.css'
 })
